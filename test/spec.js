b23729b32a2ac92aace36cf99f1688470a9d23ce
pavelzubov <pasha224@yandex.ru>
2018-08-09 09:59:52 +0300

    Move arguments to object "parameters"

--- test/spec.js
+++ test/spec.js
@@ -22,7 +22,7 @@ describe('Test library', function () {
             expect(typeof sp8de.generateSeed()).toBe('number');
         });
         it("Method generateSeed() should be returns different numbers", function () {
-            expect(sp8de.generateSeed()===sp8de.generateSeed()).toBeFalsy();
+            expect(sp8de.generateSeed() === sp8de.generateSeed()).toBeFalsy();
         });
     });
     describe("Getting random from array", function () {
@@ -30,19 +30,34 @@ describe('Test library', function () {
             expect(sp8de.getRandomFromArray).toBeDefined();
         });
         it("Method getRandomFromArray() should return array", function () {
-            expect(Array.isArray(sp8de.getRandomFromArray([1, 2, 3], 1, 6, 1))).toBeTruthy();
+            expect(Array.isArray(sp8de.getRandomFromArray({array: [1, 2, 3], min: 1, max: 6, count: 1}))).toBeTruthy();
         });
         it("Method getRandomFromArray() should return array length 3 for argument count 3", function () {
-            expect(sp8de.getRandomFromArray([1, 2, 3], 1, 6, 3).length).toEqual(3);
+            expect(sp8de.getRandomFromArray({array: [1, 2, 3], min: 1, max: 6, count: 3}).length).toEqual(3);
         });
         it("Method getRandomFromArray() should return [4, 3, 6, 4, 3, 5, 6, 3, 1, 4] for [1,2,3],1,6,10", function () {
-            expect(sp8de.getRandomFromArray([1, 2, 3], 1, 6, 10)).toEqual([4, 3, 6, 4, 3, 5, 6, 3, 1, 4]);
+            expect(sp8de.getRandomFromArray({
+                array: [1, 2, 3],
+                min: 1,
+                max: 6,
+                count: 10
+            })).toEqual([4, 3, 6, 4, 3, 5, 6, 3, 1, 4]);
         });
         it("Method getRandomFromArray() should return [240, 578, 41, 464, 688, 356, 660, 718, 830, 541] for [123,234,345],1,1000,10", function () {
-            expect(sp8de.getRandomFromArray([123, 234, 345], 1, 1000, 10)).toEqual([240, 578, 41, 464, 688, 356, 660, 718, 830, 541]);
+            expect(sp8de.getRandomFromArray({
+                array: [123, 234, 345],
+                min: 1,
+                max: 1000,
+                count: 10
+            })).toEqual([240, 578, 41, 464, 688, 356, 660, 718, 830, 541]);
         });
         it("Method getRandomFromArray() should return [350656, 927543, 685405, 591919, 518806, 178110, 384468, 666897, 987795, 198067] for [314134,34134143,13413413,1346756,168478,123,6,9,234,34589,123], 1, 1000000, 10", function () {
-            expect(sp8de.getRandomFromArray([314134,34134143,13413413,1346756,168478,123,6,9,234,34589,123], 1, 1000000, 10)).toEqual([350656, 927543, 685405, 591919, 518806, 178110, 384468, 666897, 987795, 198067]);
+            expect(sp8de.getRandomFromArray({
+                array: [314134, 34134143, 13413413, 1346756, 168478, 123, 6, 9, 234, 34589, 123],
+                min: 1,
+                max: 1000000,
+                count: 10
+            })).toEqual([350656, 927543, 685405, 591919, 518806, 178110, 384468, 666897, 987795, 198067]);
         });
     });
     describe("Generating keys", function () {
@@ -75,20 +90,27 @@ describe('Test library', function () {
             expect(sp8de.signMessage).toBeDefined();
         });
         it("Should be return object", function () {
-            expect(typeof sp8de.signMessage(privateKey, 1, 1)).toEqual('object');
+            expect(typeof sp8de.signMessage({
+                privateKey: privateKey,
+                seed: 1,
+                nonce: 1
+            })).toEqual('object');
         });
         it("Sign for 0xd9a5e808bc4e2d420aa16bc9069972a9f1ddaec7d2ee10021ae3ad9a899cd0ce,1,1 should be return sign 0x0e3bda0512370a303d9e14921b398d33e4f2eeb934958eea79360ad9e45ccf217d674852126f2978f779ac3f6bfe23190919b949e9e7ca62d4fc1fb3cb968ab31b", function () {
             let privateKey = "0xd9a5e808bc4e2d420aa16bc9069972a9f1ddaec7d2ee10021ae3ad9a899cd0ce";
-            expect(sp8de.signMessage(privateKey, 1, 1).sign).toEqual("0x0e3bda0512370a303d9e14921b398d33e4f2eeb934958eea79360ad9e45ccf217d674852126f2978f779ac3f6bfe23190919b949e9e7ca62d4fc1fb3cb968ab31b");
+            expect(sp8de.signMessage({
+                privateKey: privateKey,
+                seed: 1,
+                nonce: 1
+            }).sign).toEqual("0x0e3bda0512370a303d9e14921b398d33e4f2eeb934958eea79360ad9e45ccf217d674852126f2978f779ac3f6bfe23190919b949e9e7ca62d4fc1fb3cb968ab31b");
         });
     });
     describe("Validating", function () {
-
         it("Validate method to be defined", function () {
-            expect(sp8de.validate).toBeDefined();
+            expect(sp8de.validateSign).toBeDefined();
         });
         it("Should be Validate signed message", function () {
-            expect(sp8de.validate({
+            expect(sp8de.validateSign({
                 sign: '0x70613377916f67008e948aec77ac2aa846833d01049fc270a5fb2e0974bd3a2455947d8e5684d745e5e1b9c36f65a30d444fbba5009e0ccdeb4621413b03d8f31b',
                 pubKey: '0x492d0fd814940d1375225a7e10905585b72b0a8c',
                 seed: 1,
@@ -96,7 +118,7 @@ describe('Test library', function () {
             })).toBe(true);
         });
         it("Should be Validate incorrect message", function () {
-            expect(sp8de.validate({
+            expect(sp8de.validateSign({
                 sign: '0x70613377916f67008e948aec77ac2aa846833d01049fc270a5fb2e0974bd3a2455947d8e5684d745e5e1b9c36f65a30d444fbba5009e0ccdeb4621413b03d8f31b',
                 pubKey: '0x492d0fd814440d1375225a7e10905585b72b0a8c',
                 seed: 1,
@@ -107,8 +129,8 @@ describe('Test library', function () {
             let privateKey = sp8de.generatePrivateKey(),
                 pubKey = sp8de.getPubKey(privateKey),
                 seed = sp8de.generateSeed(),
-                sign = sp8de.signMessage(privateKey, seed, 1).sign;
-            expect(sp8de.validate({
+                sign = sp8de.signMessage({privateKey: privateKey, seed: seed, nonce: 1}).sign;
+            expect(sp8de.validateSign({
                 sign: sign,
                 pubKey: pubKey,
                 seed: seed,
